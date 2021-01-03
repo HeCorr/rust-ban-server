1d817f66710a27cd4666aa896133ad3dfbf22f79
HeCorr <hccaseclone@gmail.com>
2021-01-03 12:18:23 -0300

    omit gorm-specific fields from json
    make SteamID uniqueIndex on Ban struct

--- src/struct.go
+++ src/struct.go
@@ -4,8 +4,8 @@ import "gorm.io/gorm"
 
 // Ban struct
 type Ban struct {
-	gorm.Model
-	SteamID    string `json:"steamId"`
+	gorm.Model `json:"-"`
+	SteamID    string `json:"steamId" gorm:"uniqueIndex"`
 	Reason     string `json:"reason"`
 	ExpiryDate int64  `json:"expiryDate"`
 }
